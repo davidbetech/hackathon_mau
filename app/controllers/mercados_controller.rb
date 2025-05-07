17c97c60943af25ae07a3ef9b7937f29e93255ed
david betech <betech35@gmail.com>
2025-05-07 09:47:20 -0600

    controlador de mercado con metodos creados

--- app/controllers/mercados_controller.rb
+++ app/controllers/mercados_controller.rb
@@ -1,22 +1,46 @@
 class MercadosController < ApplicationController
+  before_action :set_mercado, only: [ :show, :edit, :update, :destroy ]
+  before_action :mercado_params, only: [ :create, :update ]
+
   def index
+    @mercados = Mercado.all
   end
 
   def show
   end
 
   def new
+    @mercado = Mercado.new
   end
 
   def create
+    @mercado = Mercado.new(mercado_params)
+    if @mercado.save
+      redirect_to @mercado, notice: "Mercado se creo."
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @mercado.update(mercado_params)
+      redirect_to @mercado, notice: "Mercado se actualizo."
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+  def mercado_params
+    params.require(:mercado).permit(:nombre, :alcaldia, :ubicacion)
+  end
+  def set_mercado
+    @mercado = Mercado.find(params[:id])
   end
 end
